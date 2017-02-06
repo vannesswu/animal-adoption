6773c21cffdd33f0914db33bc77e6f7bea3e6cc0
VannessWu <stjerryi@hotmail.com>
2017-02-06 20:53:33 +0800

    Initial Commit

---
+++ aniamal-adoption/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aniamal-adoption
+//
+//  Created by 吳建豪 on 2017/2/6.
+//  Copyright © 2017年 吳建豪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
